dd66c9f9771a95902007c1e31a5f0594b148ed8b
Joshua Watson <watson0117@gmail.com>
2021-03-07 10:44:58 -0700

    S263DAA
    DATA ATTRIBUTE ASSIGNMENT step 263

--- JavaScript_project1/JS/JavaScript_2.js
+++ JavaScript_project1/JS/JavaScript_2.js
@@ -1,1 +1,25 @@
 //var SP = "<br>"//This line is left over from the document.write exercises i needed something to space stuff out to make it more readable.
+
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    var Y = document.forms["myForm"]["lname"].value;
+    var Z = document.forms["myForm"]["Email"].value;
+    var ZZ = document.forms["myForm"]["phoneNumber"].value;
+    if (x == "") {
+        alert("First Name must be filled out");
+        return false;
+    }
+    if (Y == "") {
+        alert("Last Name must be filled out");
+        return false;
+    }
+    if (Z == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    if (ZZ == "") {
+        alert("Phone Number must be filled out");
+        return false;
+    }
+}
