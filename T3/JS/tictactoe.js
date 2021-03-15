ad6d936608d5cedd153d09b1e9f3a0e94a7e4f1c
Joshua Watson <watson0117@gmail.com>
2021-03-15 12:02:12 -0600

    Update tictactoe.js
    corrected and updated
    line 36 and line 38 to be specific

--- T3/JS/tictactoe.js
+++ T3/JS/tictactoe.js
@@ -1,71 +1,120 @@
-
+//this is to keep track of whose turn it is.
 let activePlayer = 'X';
+//this array stores an array of moves we use this to determin win conditions
 let selectedSquares = [];
-function placeXOrO(squareNumber) {
-    if (!selectedSquares.some(element => element.includes(squareNumber))) {
-        let select = document.getElementById(squareNumber);
-        if (activePlayer === 'X') {
-            select.style.backgroundImage = 'url("images/swordsXX.png")';
 
+//this function is for placeing a x or o in a square.
+function placeXOrO(squareNumber) {
+    //this ensures a square hasent been selected already.
+    //the .some() method is used to check eadch element of selectedSquar array
+    //to see if it contains the square number clicked on
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
+        //this var restieves the html element id that was clicked
+        let select = document.getElementById(squareNumber);
+        //this condition checks whs turn it is
+        if (activePlayer === 'X') {
+            //if activePlayer is equal to 'X' the x.png is placed in html
+            select.style.backgroundImage = 'url("images/swordsXX.png")';
+            //active player may only be x or o so if nt x it must be o
         } else {
+            //if activePlayer is equal to o the o.png is placed in html
             select.style.backgroundImage = 'url("images/shieldOO.png")';
         }
-        selectedSquares.push(squareNumber + activePlayer) ; 
+        //squareNumber and activeaPlayer are concatenated together and added to array
+        selectedSquares.push(squareNumber + activePlayer);
+        //this calles a function to check for any win conditions 
         checkWinConditions();
+        //this condition is for changing the active player
         if (activePlayer === 'X') {
+            //if active player is x change it to o
             activePlayer = 'O';
         } else {
+            //change activ player to x
             activePlayer = 'X';
         }
-        
-        audio('./media/swordplace.mp3');{
-       if(activePlayer === 'O')
+        //this function plays placment sound
+        audio('./media/swordplace.mp3');//this was where my error was i had  a { here and not below.
+        //this condition checks to see if it is the computers turn
+       if(activePlayer === 'O'){//i dident have the { here. this let the computer take over for bolth players
             disableClick();
+            //this function waits 1 second before computer places img and enables click.
             setTimeout(function (){ computersTurn(); }, 1000)
         }
+        //returning true is needed for the computersTurn() function to work
         return true;
-
+    }    
+    //this function results in a random square being selected
     function computersTurn() {
+        //this boolean is needed for our wile loop
         let success = false;
+        //this var stores a random number 0-8
         let pickASquare;
-        while(!success) {
+        // this condition allows our wile loop to keep trying trying if a square id selected already
+        while(!success){
+            //a random number between 0 and 8 is selected
             pickASquare = String(Math.floor(Math.random() * 9));
-            if (placeXOrO(pickASquare)) {
-                placeXOrO(pickASquare) ;
+            // a random number evaluated returns true, the square hasent been selected yet
+            if (placeXOrO(pickASquare)){
+                //this line calles the function
+                placeXOrO(pickASquare);
+                //this changes our boolean and ends the loop
                 success = true;
             };
         }
     }
 }
-
-
+//this function parses the selectedSquares array to search for win conditions
+//drawWinLine function is called to draw line if condition is met
 function checkWinConditions() {
+    // x 0, 1, 2 condition
     if      (arrayIncludes('0X', '1X', '2X')) { drawWinLine( 50, 100, 558, 100) }
+    //x 3, 4, 5 conditions
     else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine( 50, 304, 558, 304) }
+    //x 6, 7, 8 cond
     else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine( 50, 508, 558, 508) }
+    //x 0, 3, 6 cond
     else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine( 100, 50, 100, 558) }
+    // x 1, 4, 7 cond
     else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine( 304, 50, 304, 558) }
+    // x 2, 5, 8 cond
     else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine( 508, 50, 508, 558) }
+    // x 6, 4, 2 cond
     else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine( 100, 508, 510, 90) }
+    // x 0, 4, 8 cond
     else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine( 100, 100, 520, 520) }
+    // o 0, 1, 2 cond
     else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine( 50, 100, 558, 100) }
+    //o 3, 4, 5 cond
     else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine( 50, 304, 558, 304) }
+    // o 6, 7, 8 cond
     else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine( 50, 508, 558, 508) }
+    // O 0, 3, 6 cond
     else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine( 100, 50, 100, 558) }
+    // o 1, 4, 7
     else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine( 304, 50, 304, 558) }
+    // o 2, 5, 8 
     else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine( 508, 50, 508, 558) }
+    //o 6, 4, 2
     else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine( 100, 508, 510, 90) }
+    //o 0, 4, 8 
     else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine( 100, 100, 520, 520) }
-
+    // this condition checks for tie if none of the above conditions register and 9
+    //squares are selected the code executes
     else if (selectedSquares.length >= 9) {
+        //this functions playes the tie game sound
         audio('media/swordtie.mp3');
+        //this function sets a .3 sec time befor the resetGame is called
         setTimeout(function () { resetGame(); }, 1000);
     }
 
+    // this function checks if an array includes 3 strings. it is used to check for each win condition
     function arrayIncludes(squareA, squareB, squareC) {
+        //these 3 variables will be used to check for 3 in a row
         const a = selectedSquares.includes(squareA)
         const b = selectedSquares.includes(squareB)
         const c = selectedSquares.includes(squareC)
+        //if the 3 var we pass are all included in our array true is
+        //returned and our else if cond executes the drawWinLine function
         if (a === true && b === true && c === true) {return true}
     }
 }
@@ -126,6 +175,6 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
             }
             selectedSquares = [];
         }
-    }
+    
 
 
